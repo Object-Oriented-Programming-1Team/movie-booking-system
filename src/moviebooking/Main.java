e2113fd1a448a95b2c46c79ff67c141b5102f461
송지한 <157107006+Song-Jihan@users.noreply.github.com>
2025-11-15 16:41:16 +0900

    Feat/add show all check books (#7)
    * feat: Add GUI for MainScreen to view
    
    * feat: Add setup manager and gui at Main class and Modify GUIMain
    
    * feat: Add BaseFrame and Modify GUIMain
    
    * feat: Add booking movie frame on view
    
    * feat: Add Frames about showAllmovies and booking on view

--- src/moviebooking/Main.java
+++ src/moviebooking/Main.java
@@ -1,13 +1,30 @@
 package moviebooking;
 
+import javax.swing.SwingUtilities;
+
 import moviebooking.common.Manageable;
 import moviebooking.model.Movie;
+import moviebooking.model.Screen;
+import moviebooking.model.Screening;
 import moviebooking.service.MovieManager;
+import moviebooking.service.ScreenManager;
+import moviebooking.service.ScreeningFactory;
+import moviebooking.service.ScreeningManager;
+import moviebooking.view.GUIMain;
 
 public class Main {
     public static void main(String[] args) {
+        ScreeningFactory screeningFactory = new ScreeningFactory();
+        MovieManager movieManager = new MovieManager();
+        ScreenManager screenManager = new ScreenManager();
+        ScreeningManager screeningManager = new ScreeningManager(screeningFactory,movieManager,screenManager);
 
-//        Manageable<Movie, String> movieManager = new MovieManager();
+        movieManager.loadData("movies.txt");
+        screenManager.loadData("screens.txt");
+        screeningManager.loadData("screenings.txt");
 
+        SwingUtilities.invokeLater(() -> {
+            new GUIMain(movieManager, screenManager, screeningManager);
+        });
     }
 }
