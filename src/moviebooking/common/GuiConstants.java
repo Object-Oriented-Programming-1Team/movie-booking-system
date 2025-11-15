623186c26d2f35d34cba05244801f40f4b999599
soomin <joesu001@gmail.com>
2025-11-15 18:15:20 +0900

    feat : add MainFrame.java

---
+++ src/moviebooking/common/GuiConstants.java
@@ -0,0 +1,10 @@
+package moviebooking.common;
+
+import java.awt.*;
+
+public interface GuiConstants {
+    String ENGLISH_FONT = "Bernard MT Condensed";
+    String KOREAN_FONT = "맑은 고딕";
+    Color RED_COLOR = new Color(0xc01919);
+    Color DARK_BLUE_COLOR = new Color(0x162470);
+}
